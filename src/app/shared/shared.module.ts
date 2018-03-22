e6fa1dbfdd3bd466f02964e6488caa433d8f4745
asponda <airton.sponda@gmail.com>
2018-03-22 15:21:44 -0300

    Add avatar component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LogoComponent } from './components/logo/logo.component';
 import { InputErrorComponent } from './components/input-error/input-error.component';
+import { AvatarComponent } from './components/avatar/avatar.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule
   ],
-  declarations: [LogoComponent, InputErrorComponent],
-  exports: [LogoComponent, InputErrorComponent],
+  declarations: [LogoComponent, InputErrorComponent, AvatarComponent],
+  exports: [LogoComponent, InputErrorComponent, AvatarComponent],
   providers: []
 })
 export class SharedModule { }
