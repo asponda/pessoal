913540661c14608f49911b08758af8da3f3652ce
asponda <airton.sponda@gmail.com>
2018-03-22 13:15:48 -0300

    Ajuste tipografia

--- src/app/menu/services/menu/menu.service.ts
+++ src/app/menu/services/menu/menu.service.ts
@@ -9,6 +9,7 @@ export class MenuService {
 
   getMenuItens(): Array<MenuItem> {
     return [
+      { text: 'Menu', translateId: 'MENU.HOME', routerLink: ['/home'], active: false },
       { text: 'About', translateId: 'MENU.ABOUT', routerLink: ['/about'], active: false },
       { text: 'Skills', translateId: 'MENU.SKILLS', routerLink: ['/skills'], active: false },
       { text: 'Contact', translateId: 'MENU.CONTACT', routerLink: ['/contact'], active: false }
