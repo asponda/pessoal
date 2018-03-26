7b8a07a12b6bb159016451ece234e1e9d3ce7fd1
asponda <airton.sponda@gmail.com>
2018-03-26 14:09:53 -0300

    Implementação about

--- src/app/pages/about/about.module.ts
+++ src/app/pages/about/about.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about/about.component';
 import { SharedModule } from '../../shared/shared.module';
+import { I18nModule } from '../../i18n/i18n.module';
 
 @NgModule({
   imports: [
     CommonModule,
     AboutRoutingModule,
-    SharedModule
+    SharedModule,
+    I18nModule
   ],
   declarations: [AboutComponent]
 })
